4149a63f673c56eefdd3e8c966ddf96d2c6f0fff
Hrpl <ponomarevd61@gmail.com>
2024-09-25 22:33:14 +0300

    Revert "добавил модели данных"
    This reverts commit 8613674e122139b852ad5323d1a4020025a54efb.

--- monosort.domain/MonosortDbContext.cs
+++ monosort.domain/MonosortDbContext.cs
@@ -18,9 +18,4 @@ public class MonosortDbContext : IdentityDbContext
 
     public virtual DbSet<Customer> Customers { get; set; }
     public virtual DbSet<CustomerAddress> CustomerAddress { get; set; }
-    public virtual DbSet<Product> Products { get; set; }
-    public virtual DbSet<ProductInventory> ProductsInventory { get; set; }
-    public virtual DbSet<ProductInventorySnapshot> ProductsInventorySnapshot { get; set;}
-    public virtual DbSet<SalesOrder> SalesOrders { get; set; }
-    public virtual DbSet<SalesOrderItem> SalesOrdersItem { get; set;}
 }
